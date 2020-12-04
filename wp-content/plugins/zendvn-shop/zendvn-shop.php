d09692f7c55990820e7bb3f463b46d6085798478
vudinhquang <vudinhquangk53@gmail.com>
2020-12-04 22:37:14 +0900

    MVC 1

---
+++ wp-content/plugins/zendvn-shop/zendvn-shop.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: ZendVN Shopping
+Plugin URI: http://www.zend.vn
+Description: Xay dung shopping don gian WP
+Author: ZendVN group
+Version: 1.0
+Author URI: http://www.zend.vn
+*/
+
+require_once 'define.php';
