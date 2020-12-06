efe58f7e62fe17147036611328c28dc3aa3b24bb
vudinhquang <vudinhquangk53@gmail.com>
2020-12-06 18:48:08 +0900

    Custom Admin menu 4

--- wp-content/plugins/zendvn-shop/public/js/admin_menu.js
+++ wp-content/plugins/zendvn-shop/public/js/admin_menu.js
@@ -3,7 +3,21 @@ jQuery(document).ready(function($){
 	$(elemt).removeClass('wp-not-current-submenu')
 			.addClass('wp-has-current-submenu wp-menu-open');
 	$(elemt + ' > a').removeClass('wp-not-current-submenu')
-			.addClass('wp-has-current-submenu wp-menu-open');
+            .addClass('wp-has-current-submenu wp-menu-open');
+        
+    var taxonomy = getURLParameter('taxonomy');
+    var post_type = getURLParameter('post_type');
+    if(post_type == 'zsproduct') {
+        if(taxonomy == 'zs_category'){
+            var url ='edit-tags.php?taxonomy=zs_category&post_type=zsproduct';
+            $(elemt + " a[href='" + url + "']").addClass('current').parent().addClass('current');
+        }
+        
+        if(taxonomy == undefined){
+            var url ='edit.php?post_type=zsproduct';
+            $(elemt + " a[href='" + url + "']").addClass('current').parent().addClass('current');
+        }
+    }
 });
 
 function getURLParameter(sParam){
