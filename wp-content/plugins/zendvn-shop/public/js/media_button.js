f2ca39a282496b46e620e96482798201a8b6bd78
vudinhquang <vudinhquangk53@gmail.com>
2020-12-08 11:59:05 +0900

    Add & Edit 4

--- wp-content/plugins/zendvn-shop/public/js/media_button.js
+++ wp-content/plugins/zendvn-shop/public/js/media_button.js
@@ -12,9 +12,41 @@ jQuery(function($){
 					library: {type: 'image'},
 					multiple: true,
 					button: {text: 'Insert pictures'}			
-				}).open();	
+                });	
+            var self = this;
+			this.window.on('select', function(){
+                var imgs = self.window.state().get('selection').toJSON();
+
+                zendvn_sp_insert_image('#zendvn-sp-zsproduct-show-images', imgs);
+			});	
 		}
-	
+        this.window.open();
 		return false;
+    }
+    
+	//zendvn-sp-zsproduct-show-images
+	function zendvn_sp_insert_image(img_content, imgs){
+		if($(imgs).length > 0){
+			$.each(imgs, function(key, obj){
+				var imgUrl =  obj.url;
+				var newImg = '';
+				
+				newImg += '<div class="content-img">';
+				newImg += '<img width="100" height="100"';
+				newImg += '	src="' + imgUrl + '">';
+				newImg += '<div>';
+				newImg += '	<a class="remove-img">Remove</a>';
+				newImg += '</div>';
+				newImg += '<div class="div-ordering">';
+				newImg += '	<input type="text" value="1" class="ordering"';
+				newImg += '		name="zendvn-sp-zsproduct-img-ordering[]"> <input type="hidden"';
+				newImg += '		name="zendvn-sp-zsproduct-img-url[]"';
+				newImg += '		value="' + imgUrl + '">';
+				newImg += '</div>';
+				newImg += '</div>';
+				
+				$(newImg).insertBefore(img_content + " .clr");
+			});
+		}
 	}
 });
