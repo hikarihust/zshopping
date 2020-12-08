f9c779218ba337ad7e02a2d6f8cf15592a03a4d8
vudinhquang <vudinhquangk53@gmail.com>
2020-12-08 10:42:40 +0900

    Add & Edit 2

--- wp-content/plugins/zendvn-shop/controllers/backend/AdminProduct.php
+++ wp-content/plugins/zendvn-shop/controllers/backend/AdminProduct.php
@@ -1,6 +1,8 @@
 <?php
 class Zendvn_Sp_AdminProduct_Controller{
 	private $_meta_box_id = 'zendvn-sp-zsproduct';
+	private $_prefix_id = 'zendvn-sp-zsproduct-';
+	private $_prefix_key = '_zendvn_sp_zsproduct_';
 
 	public function __construct(){
 		global $zController;
@@ -8,7 +10,11 @@ class Zendvn_Sp_AdminProduct_Controller{
 		add_action('init', array($model,'create'));
 		add_filter( 'post_updated_messages', array($model,'zsproduct_updated_messages'));
 		add_filter( 'bulk_post_updated_messages', array($model,'filter_bulk_zsproduct_updated_messages'), 10, 2  ); 
-		add_action('add_meta_boxes', array($this,'display'));
+		if($zController->getParams('post_type') === 'zsproduct') {
+			add_action('add_meta_boxes', array($this,'display'));
+
+			add_action('admin_enqueue_scripts', array($this,'add_css_file'));
+		}
 	}
 
 	public function display(){
@@ -17,6 +23,22 @@ class Zendvn_Sp_AdminProduct_Controller{
 
 	public function product_images(){
 		global $zController;
+		$zController->_data['controller'] = $this;
 		$zController->getView('product/product_images.php','/backend');
+	}
+
+	public function add_css_file(){
+		global $zController;
+		
+		wp_register_style('zendvn_sp_product_product_bk', $zController->getCssUrl('product-bk.css'), array(),'1.0');
+		wp_enqueue_style('zendvn_sp_product_product_bk');
+	}
+
+	public function create_id($val){
+		return $this->_prefix_id . $val;
+	}
+	
+	public function create_key($val){
+		return $this->_prefix_key . $val;
 	}
 }
--- wp-content/plugins/zendvn-shop/templates/backend/product/product_images.php
+++ wp-content/plugins/zendvn-shop/templates/backend/product/product_images.php
@@ -1,1 +1,25 @@
 <?php
+global $zController;
+$controller = $zController->_data['controller'];
+
+$htmlObj = new ZendvnHtml();
+//=====================================================
+//Tao phan tu chua Button
+//=====================================================
+$inputID 	= $controller->create_id('button');
+$inputName 	= $controller->create_id('button');
+$inputValue = translate('Media Library Image');
+$arr 		= array('class' =>'button-secondary','id' => $inputID);
+$options	= array('type'=>'button');
+echo $btnMedia	= $htmlObj->pTag($htmlObj->button($inputName,$inputValue,$arr,$options));
+
+//=====================================================
+//Tao phan tu chua rotate360
+//=====================================================
+$inputID 	= $controller->create_id('rotate360');
+$inputName 	= $controller->create_id('rotate360');
+$inputValue = get_post_meta($post->ID,$controller->create_key('rotate360'),true);
+$arr 		= array('id' => $inputID,'rows'=>6, 'cols'=>60);
+$html		= $htmlObj->label(translate('Rotate 360')) . '<br/>'
+            . $htmlObj->textarea($inputName,$inputValue,$arr);
+echo $htmlObj->pTag($html);
