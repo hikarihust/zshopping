d1beaabe7826caaf1c71decb058276484c30b116
vudinhquang <vudinhquangk53@gmail.com>
2020-12-09 14:14:24 +0900

    Modify & Page Template 1

--- wp-content/plugins/zendvn-shop/frontend.php
+++ wp-content/plugins/zendvn-shop/frontend.php
@@ -2,6 +2,8 @@
 class Zendvn_Sp_Frontend{
 	
 	public function __construct(){
-
+		global $zController;
+		add_action('init', array($zController->getModel('Category'),'create'));
+		add_action('init', array($zController->getModel('Product'),'create'));
 	}
 }
