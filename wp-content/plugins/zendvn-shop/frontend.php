9463b54a6b7b2d1472e38aae2dd7c4911c33165a
vudinhquang <vudinhquangk53@gmail.com>
2020-12-10 15:00:22 +0900

    Show products 1

--- wp-content/plugins/zendvn-shop/frontend.php
+++ wp-content/plugins/zendvn-shop/frontend.php
@@ -5,5 +5,21 @@ class Zendvn_Sp_Frontend{
 		global $zController;
 		add_action('init', array($zController->getModel('Category'),'create'));
 		add_action('init', array($zController->getModel('Product'),'create'));
+		add_filter('template_include', array($this,'load_template'));
+	}
+
+	public function load_template($templates) {
+		global $post;
+
+		if(is_page() == 1){
+			$page_template = get_post_meta($post->ID,'_wp_page_template', true);
+			
+			echo $file = ZENDVN_SP_TEMPLATE_PATH . DS . 'frontend' . DS . $page_template;
+			if(file_exists($file)){
+				return $file;
+			}
+		}
+		
+		return $templates;
 	}
 }
