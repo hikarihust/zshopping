83836df415c9f3155d3a2d9c1a8620adb9e048e7
vudinhquang <vudinhquangk53@gmail.com>
2020-12-11 16:13:07 +0900

    Show products 7

--- wp-content/plugins/zendvn-shop/define.php
+++ wp-content/plugins/zendvn-shop/define.php
@@ -21,4 +21,5 @@ define('ZENDVN_SP_VALIDATE_PATH'		, ZENDVN_SP_PLUGIN_PATH . 'validates');
 
 //====================ORTHER==========================
 define('ZENDVN_SP_PRODUCT_URL'		, ZENDVN_SP_PUBLIC_URL . '/products');
+define('ZENDVN_SP_PRODUCT_PATH'		, ZENDVN_SP_PUBLIC_PATH . '/products');
 define('ZENDVN_SP_PREFIX'			, 'Zendvn_Sp_');
--- wp-content/plugins/zendvn-shop/helpers/ImgThumbnail.php
+++ wp-content/plugins/zendvn-shop/helpers/ImgThumbnail.php
@@ -17,10 +17,39 @@ class Zendvn_Sp_ImgThumbnail_Helper{
 				$image = wp_get_attachment_image_src($thumbnail_id,'single-post-thumbnail');
 				if(is_array($image)) $img = $image[0];
 			}else if($imgType == 'resize' ){
-
+				$image = wp_get_attachment_image_src($thumbnail_id,'single-post-thumbnail');
+				if(!is_array($image)) {
+					return $img;
+				}else{
+					$img = $this->resize($image[0],$options['width'],$options['height']);
+				}
 			}
 		}
 			
 		return $img;
-	}
+    }
+    
+    public function resize($imgURL =  null, $width = 0, $height = 0) {
+		$tmp = explode('/wp-content/', $imgURL);
+		$imgDir =ABSPATH . 'wp-content/' . $tmp[1];
+				
+        //Lay ra ten cua hinh anh hien thoi
+        //http://zshopping.xyz/wp-content/uploads/2020/12/iPhone-6.jpg -> iPhone-6.jpg
+		preg_match("/[^\/|\\\]+$/", $imgURL,$currentName);
+		$currentName = $currentName[0];
+		
+		//Duong dan thu muc de luu hinh anh moi
+		$storeFolder = ZENDVN_SP_PRODUCT_PATH . '/p' . $width .'x' . $height;
+		if(!file_exists($storeFolder)){
+			mkdir($storeFolder,0755); //CMOD
+		}
+		
+		$newImgPath = $storeFolder . '/' . $currentName;
+		if(!file_exists($newImgPath)){
+
+		}
+		$newImgUrl = ZENDVN_SP_PRODUCT_URL . '/p' . $width .'x' . $height . '/' . $currentName;
+		
+		return $newImgUrl;
+    }
 }
